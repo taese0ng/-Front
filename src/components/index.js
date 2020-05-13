8bf44f2b930b864ff13e869ed337d8cb0dc39214
taese0ng <taese0ng@naver.com>
2020-05-13 18:32:43 +0900

    Revert "2020/5/12 너의 일정을 짜고싶어 프론트 업로드"
    This reverts commit 735917a6626f8b2977d01ad8c80d5eb9b2094da4.

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Banner from './Banner';
-import MenuBar from './Menu_bar';
-import SearchBar from './Search_bar';
-import Title from './Title';
-
-export {Banner, MenuBar, SearchBar, Title};
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import Home from './Home';
-import Login from './Login';
-import MyPages from './MyPages';
-import SharePage from './SharePage';
-import SignUp from './SignUp';
-import Tendency from './Tendency';
-
-export {Home, Login, MyPages, SharePage, SignUp, Tendency};
