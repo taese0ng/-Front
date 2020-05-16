1f290008def354d2c60e689bdf9c1d8e221001be
jeong3bin <jn8121@naver.com>
2020-05-17 04:07:21 +0900

    2020-5-17 Timeline 세부화 *컴포넌트가 옆에 위치하도록 css변경 해야함 (현재 margin 등으로 처리되어있음)

---
+++ src/timeline/Place.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Place.scss";
+import Content from "./Content"
+
+// 타임라인 위 동그라미 + 말풍선(안에 내용 -> Content)
+function Place() {
+    return (
+        <div>
+            <li>
+                <div className="place__circle"></div>
+                <Content/>
+            </li>
+        </div>
+
+    );
+}
+
+export default Place;
---
+++ src/timeline/Timeline.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Place from "./Place"
+import HowTo from "./HowTo"
+import './Timeline.scss';
+
+
+// 타임 라인
+function TimeLine() {
+   return (
+      <div className="container">
+         <ul className="timeline">
+            <Place />
+
+         </ul>
+      </div>
+
+   );
+}
+
+export default TimeLine;
