9acd436ad7d18850615c25202dfa416e85f282ad
taese0ng <taese0ng@naver.com>
2020-06-09 22:44:56 +0900

    2020/6/9 review CRD 완료 U는 안할거임.!

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,32 +1,64 @@
-import React, {Component} from'react'
+import React, {useState} from'react'
 import '../css/Review.scss'
 import Rater from 'react-rater'
 import 'react-rater/lib/react-rater.css'
 
-class Review extends Component{
-    render(){
-        return(
-            <div id='review'>
-                <div>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                    <Rater color="red" total={5} rating={2} />
-                    <button className="middleBtn" id="reviewBtn">작성</button>
-                </div>
-                <ul id='reviewContents'>
-                    <li className='reviewContent'>
+function Review(props){
+    const [comment, setComment] = useState("")
+
+    function inputComment(e){
+        setComment(e.target.value)
+    }
+    return(
+        <div id='review'>
+            <div>
+                <textarea onChange={inputComment} id="textArea" cols="30" rows="10"></textarea>
+                <Rater color="red" total={5} rating={2} />
+                <button onClick={()=>{
+                    props.submitMethod(comment)
+                    const textArea = document.querySelector("#textArea")
+                    textArea.value = ""
+                    setComment("")
+                }} className="middleBtn" id="reviewBtn">작성</button>
+            </div>
+            <ul id='reviewContents'>
+            {props.commentsInfo.length === 0 ? 
+            <>
+                <li className='reviewContent'>
+                    <ul>
+                        <li id="reviewTitle">
+                            작성자가 없습니다.
+                        </li>
+                        
+                        <li id='reviewContent'>
+                            내용이 없습니다.
+                        </li>
+                    </ul>
+                </li>
+            </>:
+            <>
+                {props.commentsInfo.map((element)=>(
+                    <li className='reviewContent' key={element._id}>
                         <ul>
                             <li id="reviewTitle">
-                                제목 날짜
+                                {element.author.name}
+                                <button className="middleBtn" id="reviewBtn" 
+                                onClick={()=>props.delComment(element._id)}>
+                                    삭제
+                                </button>
                             </li>
+                            
                             <li id='reviewContent'>
-                                내용
+                                {element.comment}
                             </li>
                         </ul>
                     </li>
-                </ul>
-            </div>
-        )
-    }
+                ))}
+            </>
+            }
+            </ul>
+        </div>
+    )
 }
 
 export default Review;
