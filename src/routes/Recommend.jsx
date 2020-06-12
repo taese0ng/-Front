436624308c3b3b9073599ba2ea5820d7cbd270ec
taese0ng <taese0ng@naver.com>
2020-06-12 23:52:59 +0900

    2020/6/12 파일 네이밍 수정 해야함.

--- src/routes/Recommend.jsx
+++ src/routes/Recommend.jsx
@@ -4,11 +4,9 @@ import {Timeline_R} from '../components'
 
 class Recommend extends Component{
     render(){
-        const area = this.props.match.params.area;
-        console.log("추천시작",area)
         return(
             <div style={{textAlign:`center`}} className="footer__height">
-                <Timeline_R info={area}/>
+                <Timeline_R/>
             </div>
         )
     }
