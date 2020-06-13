4301848dc119c67bab21cbe132fe032012c44379
jeong3bin <jn8121@naver.com>
2020-06-14 01:17:00 +0900

    2020-06-14 맵에 뜨는 아이콘 및 색 바꾸기 setBounds 오류 아마 고침

--- src/components/ShowRoute_R.jsx
+++ src/components/ShowRoute_R.jsx
@@ -2,20 +2,21 @@
 
 import React, { Component } from 'react';
 import '../css/ShowRoute.scss';
-import { kakaoMapsKey } from '../key'
+import { kakaoMapsKey } from '../key';
+import icon from '../assets/icon_howto.png';
 
 let map;
 let script;
 var bounds;
 
 class ShowRoute_R extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-           polyline : [],
-           markers : []
+            polyline: [],
+            markers: []
         }
-      }
+    }
 
     onUnload(event) {
         alert('page Refreshed')
@@ -23,32 +24,39 @@ class ShowRoute_R extends Component {
 
     componentDidUpdate() {
 
-        const {recommend} = this.props
+        const { recommend } = this.props
 
         // console.log(recommend, "여기다");
         this.state.polyline.forEach(elem => {
             elem.setMap(null);
         })
-        this.state.markers.forEach(elem =>{
+        this.state.markers.forEach(elem => {
             elem.setMap(null);
         })
 
         recommend.map((elem, index) => {
+            var markerImage = new kakao.maps.MarkerImage(
+                `${icon}`, new kakao.maps.Size(52, 72),new kakao.maps.Point(26, 36));
             if (recommend.length > index + 1) {
                 var to = new kakao.maps.LatLng(recommend[index + 1].lat, recommend[index + 1].lng);
                 var from = new kakao.maps.LatLng(recommend[index].lat, recommend[index].lng);
+
+
+                
+
                 let markerTemp = new kakao.maps.Marker({
                     map: map, // 마커를 표시할 지도
+                    image: markerImage,
                     position: from, // 마커를 표시할 위치
                     title: index + 1 // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                 });
 
                 let polylineTemp = new kakao.maps.Polyline({
                     path: [from, to], // 선을 구성하는 좌표배열 입니다
-                    strokeWeight: 5, // 선의 두께 입니다
-                    strokeColor: 'red', // 선의 색깔입니다
-                    strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-                    strokeStyle: 'dashed' // 선의 스타일입니다
+                    strokeWeight: 7, // 선의 두께 입니다
+                    strokeColor: '#031EA8', // 선의 색깔입니다
+                    strokeOpacity: 0.65, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+                    strokeStyle: 'solid' // 선의 스타일입니다
 
                 });
 
@@ -59,11 +67,12 @@ class ShowRoute_R extends Component {
 
                     markerTemp = new kakao.maps.Marker({
                         map: map, // 마커를 표시할 지도
+                        image: markerImage,
                         position: to, // 마커를 표시할 위치
                         title: index + 2 // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                     });
-                    
-                    this.state.markers.push(markerTemp);   
+
+                    this.state.markers.push(markerTemp);
                 }
 
                 bounds.extend(to);
@@ -77,10 +86,12 @@ class ShowRoute_R extends Component {
                     window.location.reload();
                     //react가 먼저 사라져서 kakako script에서  오류나는 거니까 reload해줌
                 }
+
+                map.setBounds(bounds);
                 return null;
             }
+            return null;
 
-            return map.setBounds(bounds);
         });
     }
 
