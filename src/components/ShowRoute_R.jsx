e962cde3c52a962dcc65bd05a64986884df31790
jeong3bin <jn8121@naver.com>
2020-06-16 15:47:52 +0900

    2020-06-16 추천 결과 없을 시 예외처리

--- src/components/ShowRoute_R.jsx
+++ src/components/ShowRoute_R.jsx
@@ -7,7 +7,7 @@ import icon from '../assets/icon_howto.png';
 
 let map;
 let script;
-
+let bounds;
 class ShowRoute_R extends Component {
     constructor(props) {
         super(props);
@@ -19,76 +19,74 @@ class ShowRoute_R extends Component {
     componentDidUpdate() {
 
         const { recommend } = this.props
+        if (window.kakao !== undefined && recommend !== undefined) {
+            this.state.polyline.forEach(elem => {
+                elem.setMap(null);
+            })
+            this.state.markers.forEach(elem => {
+                elem.setMap(null);
+            })
 
+            bounds = new window.kakao.maps.LatLngBounds();
 
-        // console.log(recommend, "여기다");
-        this.state.polyline.forEach(elem => {
-            elem.setMap(null);
-        })
-        this.state.markers.forEach(elem => {
-            elem.setMap(null);
-        })
+            recommend.map((elem, index) => {
+                let size = new window.kakao.maps.Size(52, 72);
+                let markerImage = new window.kakao.maps.MarkerImage(
+                    `${icon}`, size, new window.kakao.maps.Point(26, 36));
 
+                if (recommend.length > index + 1) {
+                    var to = new window.kakao.maps.LatLng(recommend[index + 1].lat, recommend[index + 1].lng);
+                    var from = new window.kakao.maps.LatLng(recommend[index].lat, recommend[index].lng);
 
-        var bounds = new window.kakao.maps.LatLngBounds();
-
-        recommend.map((elem, index) => {
-            let size = new window.kakao.maps.Size(52, 72);
-            let markerImage = new window.kakao.maps.MarkerImage(
-                `${icon}`, size, new window.kakao.maps.Point(26, 36));
-
-            if (recommend.length > index + 1) {
-                var to = new window.kakao.maps.LatLng(recommend[index + 1].lat, recommend[index + 1].lng);
-                var from = new window.kakao.maps.LatLng(recommend[index].lat, recommend[index].lng);
-
-                let markerTemp = new window.kakao.maps.Marker({
-                    map: map, // 마커를 표시할 지도
-                    image: markerImage,
-                    position: from, // 마커를 표시할 위치
-                    title: index + 1 // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                });
-
-                let polylineTemp = new window.kakao.maps.Polyline({
-                    path: [from, to], // 선을 구성하는 좌표배열 입니다
-                    strokeWeight: 7, // 선의 두께 입니다
-                    strokeColor: '#031EA8', // 선의 색깔입니다
-                    strokeOpacity: 0.65, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-                    strokeStyle: 'solid' // 선의 스타일입니다
-
-                });
-
-                this.state.polyline.push(polylineTemp);
-                this.state.markers.push(markerTemp);
-
-                if (index === recommend.length - 2) {
-
-                    markerTemp = new window.kakao.maps.Marker({
+                    let markerTemp = new window.kakao.maps.Marker({
                         map: map, // 마커를 표시할 지도
                         image: markerImage,
-                        position: to, // 마커를 표시할 위치
-                        title: index + 2 // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                        position: from, // 마커를 표시할 위치
+                        title: index + 1 // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                     });
 
+                    let polylineTemp = new window.kakao.maps.Polyline({
+                        path: [from, to], // 선을 구성하는 좌표배열 입니다
+                        strokeWeight: 7, // 선의 두께 입니다
+                        strokeColor: '#031EA8', // 선의 색깔입니다
+                        strokeOpacity: 0.65, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+                        strokeStyle: 'solid' // 선의 스타일입니다
+
+                    });
+
+                    this.state.polyline.push(polylineTemp);
                     this.state.markers.push(markerTemp);
+
+                    if (index === recommend.length - 2) {
+
+                        markerTemp = new window.kakao.maps.Marker({
+                            map: map, // 마커를 표시할 지도
+                            image: markerImage,
+                            position: to, // 마커를 표시할 위치
+                            title: index + 2 // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                        });
+
+                        this.state.markers.push(markerTemp);
+                    }
+
+                    bounds.extend(to);
+                    bounds.extend(from);
+
+                    //오류 해결
+                    try {
+                        polylineTemp.setMap(map);
+                    } catch (err) {
+                        console.log(`error 발생 ~! \n${err}`);
+                        window.location.reload();
+                        //react가 먼저 사라져서 kakako script에서  오류나는 거니까 reload해줌
+                    }
+
+                    map.setBounds(bounds);
+                    return null;
                 }
-
-                bounds.extend(to);
-                bounds.extend(from);
-
-                //오류 해결
-                try {
-                    polylineTemp.setMap(map);
-                } catch (err) {
-                    console.log(`error 발생 ~! \n${err}`);
-                    window.location.reload();
-                    //react가 먼저 사라져서 kakako script에서  오류나는 거니까 reload해줌
-                }
-
-                map.setBounds(bounds);
                 return null;
-            }
-            return null;
-        });
+            });
+        }
 
     }
 
--- src/components/Timeline_R.jsx
+++ src/components/Timeline_R.jsx
@@ -5,112 +5,112 @@ import { connect } from "react-redux";
 import axios from 'axios';
 import ShowRouteR from "./ShowRoute_R.jsx";
 import update from 'react-addons-update';
-import {ServerIP, HopeIP} from '../key'
-import {setSchedule, initSchedule} from "../store/store";
+import { ServerIP, HopeIP } from '../key'
+import { setSchedule, initSchedule } from "../store/store";
 import { Areas } from '../components/Areas.js'
 import Search from './Search.jsx'
 import Dialog from "./Dialog.jsx";
 
 class Timeline_R extends Component {
-   constructor(props){
+   constructor(props) {
       super(props);
       this.state = {
          title: '',
-         description : '',
-         routes : [],
-         bigData : [],
-         reviseBtn : false,
-         nowScheduleIdx : 0,
-         temp : [],
-         openSearch:false,
-         addIdx : 0,
-         openAdopt : false
+         description: '',
+         routes: [],
+         bigData: [],
+         reviseBtn: false,
+         nowScheduleIdx: 0,
+         temp: [],
+         openSearch: false,
+         addIdx: 0,
+         openAdopt: false
       }
-    }
+   }
 
-   UNSAFE_componentWillMount(){
-      const {setSchedule,initSchedule, AreaCodes} = this.props
+   UNSAFE_componentWillMount() {
+      const { setSchedule, initSchedule, AreaCodes } = this.props
       const userID = JSON.parse(sessionStorage.getItem("user"))._id
       initSchedule();
       AreaCodes.map((AreaCode) => (
          axios.get(`${HopeIP}/api/recommend/user?userId=${userID}&areaCode=${AreaCode.areaCode}&sigunguCode=${AreaCode.sigunguCode}`)
-         .then(res => {
-            // console.log(res);
-            res.data.forEach(schedule=>{
-               let routeList = []
-               schedule.forEach( element => {
-                  let data = element;
-                  setSchedule(data.title);
-                  routeList.push({
-                     contentId : data.contentId,
-                     name: data.title,
-                     overview : data.overview,
-                     image : data.firstImage,
-                     homepage : data.homepage,
-                     title : data.title,
-                     zipCode : data.zipCode,
-                     tel : data.tel,
-                     lat:data.mapY,
-                     lng:data.mapX,
-                     description : data.overview
-                  })
-                  
-               });
+            .then(res => {
+                console.log("추천결과",res);
+               res.data.forEach(schedule => {
+                  let routeList = []
+                  schedule.forEach(element => {
+                     let data = element;
+                     setSchedule(data.title);
+                     routeList.push({
+                        contentId: data.contentId,
+                        name: data.title,
+                        overview: data.overview,
+                        image: data.firstImage,
+                        homepage: data.homepage,
+                        title: data.title,
+                        zipCode: data.zipCode,
+                        tel: data.tel,
+                        lat: data.mapY,
+                        lng: data.mapX,
+                        description: data.overview
+                     })
 
-               this.setState({
-                  bigData: update(
-                  this.state.bigData,
-                  {
-                     $push: [routeList]
+                  });
+
+                  this.setState({
+                     bigData: update(
+                        this.state.bigData,
+                        {
+                           $push: [routeList]
+                        })
                   })
+
+
                })
-
-
+               this.setState({
+                  nowScheduleIdx: 0,
+                  routes: this.state.bigData[0]
+               })
             })
-            this.setState({
-               nowScheduleIdx : 0,
-               routes : this.state.bigData[0]
+            .catch(err => {
+               console.log("츠천에러", err);
             })
-         })
-         .catch(err => {
-            console.log("츠천에러",err);
-         })
       ))
    }
 
    clickRevise = () => {
       this.setState({
-         temp : this.state.routes,
-         reviseBtn : !this.state.reviseBtn
+         temp: this.state.routes,
+         reviseBtn: !this.state.reviseBtn
       });
    }
 
-   clickUpBtn = (idx) =>{
-      if(idx > 0){
+   clickUpBtn = (idx) => {
+      if (idx > 0) {
          let down = this.state.routes[idx];
-         let up = this.state.routes[idx-1];
+         let up = this.state.routes[idx - 1];
          this.setState({
-            routes : update(
+            routes: update(
                this.state.routes,
                {
-                  [idx] : {$set : up},
-                  [idx-1]: {$set: down}
+                  [idx]: { $set: up },
+                  [idx - 1]: { $set: down }
                }
             )
          })
       }
    }
 
-   clickDownBtn = (idx) =>{
-      if(idx < this.state.routes.length-1){
+   clickDownBtn = (idx) => {
+      if (idx < this.state.routes.length - 1) {
          let up = this.state.routes[idx];
-         let down = this.state.routes[idx+1];
+         let down = this.state.routes[idx + 1];
          this.setState({
-            routes : update(
+            routes: update(
                this.state.routes,
                {
-                  [idx] : {$set : down},
-                  [idx+1]: {$set: up}
+                  [idx]: { $set: down },
+                  [idx + 1]: { $set: up }
                }
             )
          })
@@ -119,18 +119,18 @@ class Timeline_R extends Component {
 
    clickDelBtn = (idx) => {
       this.setState({
-         routes : update(
+         routes: update(
             this.state.routes,
             {
-               $splice: [[idx,1]]
+               $splice: [[idx, 1]]
             }
          )
       })
    }
 
    clickUpdateBtn = () => {
-      const {setSchedule,initSchedule} = this.props;
-      
+      const { setSchedule, initSchedule } = this.props;
+
       // 일정 수정 시 업데이트
       initSchedule();
       this.state.routes.forEach(element => {
@@ -138,181 +138,184 @@ class Timeline_R extends Component {
       })
 
       this.setState({
-         reviseBtn : !this.state.reviseBtn
+         reviseBtn: !this.state.reviseBtn
       });
-      
+
       this.setState({
-         bigData: this.state.bigData.slice(0,this.state.nowScheduleIdx).concat([this.state.routes]).concat(this.state.bigData.slice(this.state.nowScheduleIdx+1, this.state.bigData.length))
+         bigData: this.state.bigData.slice(0, this.state.nowScheduleIdx).concat([this.state.routes]).concat(this.state.bigData.slice(this.state.nowScheduleIdx + 1, this.state.bigData.length))
       })
    }
 
    nextSchedule = () => {
-      if(this.state.nowScheduleIdx < this.state.bigData.length){
+      if (this.state.nowScheduleIdx < this.state.bigData.length) {
          this.setState({
-            nowScheduleIdx : this.state.nowScheduleIdx+1,
-            routes : this.state.bigData[this.state.nowScheduleIdx+1]
+            nowScheduleIdx: this.state.nowScheduleIdx + 1,
+            routes: this.state.bigData[this.state.nowScheduleIdx + 1]
          })
       }
    }
 
    prevSchedule = () => {
-      if(this.state.nowScheduleIdx > 0){
+      if (this.state.nowScheduleIdx > 0) {
          this.setState({
-            nowScheduleIdx : this.state.nowScheduleIdx-1,
-            routes : this.state.bigData[this.state.nowScheduleIdx-1]
+            nowScheduleIdx: this.state.nowScheduleIdx - 1,
+            routes: this.state.bigData[this.state.nowScheduleIdx - 1]
          })
       }
    }
 
-   modifiCancel = () =>{
+   modifiCancel = () => {
       this.setState({
-         routes : this.state.temp,
-         reviseBtn : !this.state.reviseBtn
+         routes: this.state.temp,
+         reviseBtn: !this.state.reviseBtn
       })
    }
 
-   mySchedule = () =>{
-      const {AreaCodes, selectDate} = this.props
+   mySchedule = () => {
+      const { AreaCodes, selectDate } = this.props
       let scheduleName = ""
-      for(let i=0; i<AreaCodes.length; i++){
-         for(let j=0; j<Areas.length; j++){
-            if(AreaCodes[i].areaCode === Areas[j].code){
-               if(AreaCodes[i].sigunguCode=== 0){
+      for (let i = 0; i < AreaCodes.length; i++) {
+         for (let j = 0; j < Areas.length; j++) {
+            if (AreaCodes[i].areaCode === Areas[j].code) {
+               if (AreaCodes[i].sigunguCode === 0) {
                   scheduleName += Areas[j].name
                   break
                }
-               for(let k=0; k<Areas[j].areas.length; k++){
-                  if(Areas[j].areas[k].code === AreaCodes[i].sigunguCode){
+               for (let k = 0; k < Areas[j].areas.length; k++) {
+                  if (Areas[j].areas[k].code === AreaCodes[i].sigunguCode) {
                      scheduleName += Areas[j].areas[k].name
                      break
                   }
                }
-               if(i === AreaCodes.length-1){
+               if (i === AreaCodes.length - 1) {
                   break;
-               }else{
-                  scheduleName+=", "
+               } else {
+                  scheduleName += ", "
                }
             }
          }
       }
 
       let contentIds = []
-      this.state.routes.forEach(el=>{
+      this.state.routes.forEach(el => {
          contentIds.push(el.contentId)
       })
-      axios.post(`${ServerIP}/itinerary/upload`,{
+      axios.post(`${ServerIP}/itinerary/upload`, {
          areaCodes: AreaCodes,
          date: selectDate,
          title: scheduleName,
          description: this.state.routes.description,
          routes: contentIds
-       },{
-         headers:{
-            'Authorization' : `Bearer ${sessionStorage.getItem('token')}`
+      }, {
+         headers: {
+            'Authorization': `Bearer ${sessionStorage.getItem('token')}`
          }
-      }).then(res=>{
-         
+      }).then(res => {
+
       }).catch(err => console.log(err))
    }
 
 
    clickAddBtn = (idx) => {
-      this.setState({addIdx : idx})
+      this.setState({ addIdx: idx })
       const timeline = document.querySelector("body")
       const Search = document.querySelector("#Search")
-      if(!this.state.openSearch){
-         timeline.style.overflow="hidden";
-         Search.style.top = document.documentElement.scrollTop+"px";
+      if (!this.state.openSearch) {
+         timeline.style.overflow = "hidden";
+         Search.style.top = document.documentElement.scrollTop + "px";
       }
-      else{
-         timeline.style.overflow="unset";
+      else {
+         timeline.style.overflow = "unset";
       }
       this.setState({
-         openSearch : !this.state.openSearch
+         openSearch: !this.state.openSearch
       })
    }
 
-   addSchedule = (info) =>{
+   addSchedule = (info) => {
       // console.log(info)
       const obj = {
-         contentId : info.contentId,
+         contentId: info.contentId,
          name: info.title,
-         overview : info.overview,
-         image : info.firstImage,
-         homepage : info.homepage,
-         title : info.title,
-         zipCode : info.zipCode,
-         tel : info.tel,
-         lat:info.mapY,
-         lng:info.mapX,
-         description : info.overview
+         overview: info.overview,
+         image: info.firstImage,
+         homepage: info.homepage,
+         title: info.title,
+         zipCode: info.zipCode,
+         tel: info.tel,
+         lat: info.mapY,
+         lng: info.mapX,
+         description: info.overview
       }
-      
+
       this.setState({
-         routes: this.state.routes.slice(0,this.state.addIdx).concat(obj).concat(this.state.routes.slice(this.state.addIdx,this.state.routes.length))
+         routes: this.state.routes.slice(0, this.state.addIdx).concat(obj).concat(this.state.routes.slice(this.state.addIdx, this.state.routes.length))
       })
    }
 
-   clickAdopt = () =>{
+   clickAdopt = () => {
       const timeline = document.querySelector("body")
       const dialog = document.querySelector("#Dialog")
-      if(!this.state.openAdopt){
-         timeline.style.overflow="hidden";
-         dialog.style.top = document.documentElement.scrollTop+"px";
+      if (!this.state.openAdopt) {
+         timeline.style.overflow = "hidden";
+         dialog.style.top = document.documentElement.scrollTop + "px";
       }
-      else{
-         timeline.style.overflow="unset";
+      else {
+         timeline.style.overflow = "unset";
       }
       this.setState({
-         openAdopt : !this.state.openAdopt
+         openAdopt: !this.state.openAdopt
       })
    }
 
 
-   render(){
+   render() {
+      console.log(this.state.routes)
       return (
          <div className="container">
-            <ShowRouteR recommend={this.state.routes}/>
-            <Search clickAddBtn={this.clickAddBtn} addSchedule={this.addSchedule} className={!this.state.openSearch ? "notVisible" : ""}/>
-            <Dialog onClickOk = {this.mySchedule} onCancel={this.clickAdopt} className={!this.state.openAdopt ? "notVisible" : ""} comment = "내 일정으로 채택하시겠습니까?"/> 
-            <ul className="timeline">
-               {
-                  this.state.routes.map((route,index) => (
-                     <li key={index} className="route">
-                        {
-                           this.state.reviseBtn &&
-                           <div>
-                              <button className="smallBtn" onClick={() => this.clickUpBtn(index)}>Up</button>
-                              <button className="smallBtn" onClick={() => this.clickDownBtn(index)}>Down</button>
-                              <button className="smallBtn" onClick={() => this.clickDelBtn(index)}>Delete</button>
-                              <button className="smallBtn" onClick={() => this.clickAddBtn(index)}>Add</button>
-                           </div>
-                        }
-                        <Place info={route} index={index} mapState={!this.state.reviseBtn}/>
-                     </li>
-                  ))
-               }
-            </ul>
+            <ShowRouteR recommend={this.state.routes} />
+            <Search clickAddBtn={this.clickAddBtn} addSchedule={this.addSchedule} className={!this.state.openSearch ? "notVisible" : ""} />
+            <Dialog onClickOk={this.mySchedule} onCancel={this.clickAdopt} className={!this.state.openAdopt ? "notVisible" : ""} comment="내 일정으로 채택하시겠습니까?" />
+            {(this.state.routes !== undefined) ?
+               <ul className="timeline">
+                  {
+                     this.state.routes.map((route, index) => (
+                        <li key={index} className="route">
+                           {
+                              this.state.reviseBtn &&
+                              <div>
+                                 <button className="smallBtn" onClick={() => this.clickUpBtn(index)}>Up</button>
+                                 <button className="smallBtn" onClick={() => this.clickDownBtn(index)}>Down</button>
+                                 <button className="smallBtn" onClick={() => this.clickDelBtn(index)}>Delete</button>
+                                 <button className="smallBtn" onClick={() => this.clickAddBtn(index)}>Add</button>
+                              </div>
+                           }
+                           <Place info={route} index={index} mapState={!this.state.reviseBtn} />
+                        </li>
+                     ))
+                  }
+               </ul>
+               :<p>추천 결과가 없습니다.</p>}
             <div>
-               {((this.state.nowScheduleIdx !== 0 ) && (!this.state.reviseBtn)) &&
+               {((this.state.nowScheduleIdx !== 0) && (!this.state.reviseBtn)) &&
                   <span>
                      <button className="middleBtn borderBtn" onClick={this.prevSchedule}>이전 추천</button>
                   </span>
                }
                <span id="ReviseSchedule">
                   {!this.state.reviseBtn ?
-                  <button className="middleBtn borderBtn scheduleBtn" onClick={this.clickRevise}>일정 수정</button> :
-                  <button className="middleBtn borderBtn scheduleBtn" onClick={this.clickUpdateBtn}>수정 완료</button>
+                     <button className="middleBtn borderBtn scheduleBtn" onClick={this.clickRevise}>일정 수정</button> :
+                     <button className="middleBtn borderBtn scheduleBtn" onClick={this.clickUpdateBtn}>수정 완료</button>
                   }
                </span>
                <span>
                   {!this.state.reviseBtn ?
                      // <Link to='/yourSchedule'><button className="middleBtn borderBtn scheduleBtn" onClick={this.mySchedule}>일정 채택</button></Link>:
-                     <button className="middleBtn borderBtn scheduleBtn" onClick={this.clickAdopt}>일정 채택</button>:
+                     <button className="middleBtn borderBtn scheduleBtn" onClick={this.clickAdopt}>일정 채택</button> :
                      <button className="middleBtn borderBtn scheduleBtn" onClick={this.modifiCancel}>수정 취소</button>
                   }
                </span>
-               {((this.state.nowScheduleIdx !== this.state.bigData.length-1) && (!this.state.reviseBtn)) &&
+               {((this.state.nowScheduleIdx !== this.state.bigData.length - 1) && (!this.state.reviseBtn)) &&
                   <span>
                      <button className="middleBtn borderBtn" onClick={this.nextSchedule}>다음 추천</button>
                   </span>
@@ -324,19 +327,19 @@ class Timeline_R extends Component {
 }
 
 function mapStateToProps(state) {
-   return { 
-      itineraryId : state.itineraryId,
-      schedule:state.schedule,
+   return {
+      itineraryId: state.itineraryId,
+      schedule: state.schedule,
       AreaCodes: state.AreaCodes,
       selectDate: state.selectDate,
-    };
- }
+   };
+}
 
- function mapDispatchToProps(dispatch) {
-   return { 
+function mapDispatchToProps(dispatch) {
+   return {
       setSchedule: (text) => dispatch(setSchedule(text)),
       initSchedule: () => dispatch(initSchedule()),
    };
- }
+}
 
-export default connect(mapStateToProps,mapDispatchToProps) (Timeline_R);
+export default connect(mapStateToProps, mapDispatchToProps)(Timeline_R);
